509af858a82d12e843b8496a707e0ac9bd76e9c5
k1LoW <k1lowxb@gmail.com>
2017-02-16 10:45:08 +0900

    Fix tableize

--- lib/tmpfk/ext/column.rb
+++ lib/tmpfk/ext/column.rb
@@ -1,10 +1,10 @@
 class ActiveRecord::ConnectionAdapters::Column
   def foreign_key?
     return false unless name.end_with?('_id')
-    ActiveRecord::Base.connection.data_source_exists?(name.humanize(capitalize: false).tableize)
+    ActiveRecord::Base.connection.data_source_exists?(name.humanize(capitalize: false).tableize.tr(' ', '_'))
   end
 
   def foreign_table
-    name.humanize(capitalize: false).tableize
+    name.humanize(capitalize: false).tableize.tableize.tr(' ', '_')
   end
 end
