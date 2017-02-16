8afcff1fd99445b95bbdbaeebd210656165a80e9
k1LoW <k1lowxb@gmail.com>
2017-02-16 13:08:38 +0900

    tweak

--- lib/tmpfk/ext/column.rb
+++ lib/tmpfk/ext/column.rb
@@ -1,7 +1,7 @@
 class ActiveRecord::ConnectionAdapters::Column
   def foreign_key?
     return false unless name.end_with?('_id')
-    ActiveRecord::Base.connection.data_source_exists?(name.humanize(capitalize: false).tableize.tr(' ', '_'))
+    ActiveRecord::Base.connection.data_source_exists?(foreign_table)
   end
 
   def foreign_table
